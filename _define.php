73ac298f4ee05d2aa389504958f2011ab4c08620
Jean-Christian Denis <contact@jcdenis.fr>
2025-09-13 10:23:44 +0200

    release 1.13.3

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Discussion',
     'Allow user to post from frontend.',
     'Jean-Christian Paul Denis and Contributors',
-    '0.13.2',
+    '0.13.3',
     [
         'requires'    => [
             ['core', '2.36'],
